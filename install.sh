d064a9b4d05724c437ef6e8220a2f9047299bbe1
RubiGuy <58630827+RubiGuy@users.noreply.github.com>
2025-12-12 10:27:09 -0300

    script to automatize

---
+++ install.sh
@@ -0,0 +1,1 @@
+pacman -S neovim git btop python sudo zsh curl zip unzip wget base-devel cmake clang jdk-openjdk mesa discord bluez libreoffice-fresh ttf-jetbrains-mono-nerd pipewire vlc audacity obs-studio gimp steam --needed
