84843d82436e1238e8aa6a148c6574d5c45d1f0f
RubiGuy <58630827+RubiGuy@users.noreply.github.com>
2025-12-13 18:59:10 -0300

    Add sudo to pacman install command

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-pacman -Sy --noconfirm --needed neovim git btop python sudo zsh curl zip unzip wget base-devel cmake clang jdk-openjdk mesa discord bluez libreoffice-fresh ttf-jetbrains-mono-nerd pipewire vlc audacity obs-studio gimp steam
+sudo pacman -Sy --noconfirm --needed neovim git btop python sudo zsh curl zip unzip wget base-devel cmake clang jdk-openjdk mesa discord bluez libreoffice-fresh ttf-jetbrains-mono-nerd pipewire vlc audacity obs-studio gimp steam
