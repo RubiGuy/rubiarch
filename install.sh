aef8070894641b36bcc65f25c2e34aa81e5d935d
RubiGuy <58630827+RubiGuy@users.noreply.github.com>
2025-12-13 19:03:32 -0300

    Update install script to include additional packages

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 sudo pacman -Sy --noconfirm --needed neovim git btop python sudo zsh curl zip unzip wget base-devel cmake clang jdk-openjdk mesa discord bluez libreoffice-fresh ttf-jetbrains-mono-nerd pipewire vlc audacity obs-studio gimp steam
+&& git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm
